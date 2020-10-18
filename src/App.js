47f4d5f34f511691562a4a870afa491cf8d3deae
Chinedu Dara <cdara@vatebra.com>
2020-10-19 00:09:24 +0100

    Added withRouter HOC for menu-item for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+
+const HatsPage = () => {
+  return <h1>Hats Page</h1>
+}
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
