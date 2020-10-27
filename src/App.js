a8de53fa58b31b6cfc51b2701ff131c79fdf2830
Chinedu Dara <cdara@vatebra.com>
2020-10-27 17:42:19 +0100

    Added collection preview, collection items and header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 import './App.css';
-
-const HatsPage = () => {
-  return <h1>Hats Page</h1>
-}
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
